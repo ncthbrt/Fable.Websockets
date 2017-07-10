2db2499243142f56cacdfb34ed8c286c793006c2
Nick Cuthbert <nick@whereismytransport.com>
2017-07-10 14:29:13 +0200

    Removed Elmish and Elmish samples to own repo so as to better unbundle dependencies

--- build.fsx
+++ build.fsx
@@ -5,6 +5,8 @@ open Fake
 open Fake.DotNetCli
 open Fake.ProcessHelper
 open Fake.FileSystem
+open Fake.YarnHelper
+
 
 // Directories
 let buildDir  = "./build/"
@@ -18,9 +20,7 @@ let appReferences  =
 
 
 let projectFolders  =  [ (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./src"))  
-                         (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./src"))
-                         (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./samples/"))
-                         (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./samples/"))
+                         (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./src"))                         
                        ] 
                        |> Array.concat
                        |> Seq.map (fun m -> m.Directory.FullName)
@@ -31,12 +31,10 @@ Target "Clean" (fun _ ->
     CleanDirs [buildDir; deployDir]
 )
 
-Target "YarnRestore" (fun _->     
-   let yarn = tryFindFileOnPath (if isUnix then "yarn" else "yarn.cmd") |> Option.get
-
-   Shell.Exec (yarn, "install", "./") |> ignore
-   Shell.Exec (yarn, "install", "./samples/HelloWorld/Client/") |> ignore
-   Shell.Exec (yarn, "install", "./src/Fable.Websockets.Client/") |> ignore
+Target "YarnRestore" (fun _->        
+   ["./"; "./src/Fable.Websockets.Client/"]
+   |> Seq.iter (fun dir -> Yarn (fun p ->{ p with Command = Install Standard; WorkingDirectory = dir}))
+   |> ignore   
 )
 
 Target "Restore" (fun _->    
@@ -53,18 +51,9 @@ Target "Build" (fun _ ->
     |> ignore        
 )
 
-Target "RunElmishSample" (fun _ ->
-    // Start client
-    [ async { return (DotNetCli.RunCommand (fun p -> {p with WorkingDir = "./samples/HelloWorld/Server/"}) "watch run") }
-      async { return (DotNetCli.RunCommand (fun p -> {p with WorkingDir = "./samples/HelloWorld/Client/"}) "fable yarn-run start-sample") }
-    ] |> Async.Parallel |> Async.RunSynchronously |> ignore
-)
-
 
 // Build order
 "Clean" ==> "Restore" ==> "YarnRestore" ==> "Build"
-
-"Build" ==> "RunElmishSample"
 
 // start build
 RunTargetOrDefault "Build"
