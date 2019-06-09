f4b0d0571670ce3e9c5436f69e8e140c8a30f259
OlegZee <olegzzzz@gmail.com>
2019-06-09 23:57:33 +0700

    upgrade to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,13 +1,11 @@
-// include Fake libs
-#r "./packages/FAKE/tools/FakeLib.dll"
+#load ".fake/build.fsx/intellisense.fsx"
 
-open Fake
-open Fake.DotNetCli
-open Fake.ProcessHelper
-open Fake.FileSystem
-open Fake.YarnHelper
-open Fake.ReleaseNotesHelper
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.JavaScript
 
+open Fake.Core.TargetOperators
 
 // Directories
 let buildDir  = "./build/"
@@ -15,41 +13,40 @@ let deployDir = "./deploy/"
 
 
 
-let projectFolders  =  [ (filesInDirMatchingRecursive "*.fsproj" (directoryInfo "./src"))  
-                         (filesInDirMatchingRecursive "*.csproj" (directoryInfo "./src"))                         
+let projectFolders  =  [ (DirectoryInfo.getMatchingFilesRecursive "*.fsproj" (DirectoryInfo.ofPath "./src"))  
+                         (DirectoryInfo.getMatchingFilesRecursive "*.csproj" (DirectoryInfo.ofPath "./src"))                         
                        ] 
                        |> Array.concat
                        |> Seq.map (fun m -> m.Directory.FullName)
 
 
-Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+Target.create "Clean" (fun _ ->
+    Shell.cleanDirs [buildDir; deployDir]
 )
 
-Target "YarnRestore" (fun _->        
+Target.create "YarnRestore" (fun _->        
    ["./"; "./src/Fable.Websockets.Client/"]
-   |> Seq.iter (fun dir -> Yarn (fun p ->{ p with Command = Install Standard; WorkingDirectory = dir}))
+   |> Seq.iter (fun dir -> Yarn.install (fun p -> { p with WorkingDirectory = dir}))
    |> ignore   
 )
 
-Target "Restore" (fun _->    
+Target.create "Restore" (fun _->    
     projectFolders
-    |> Seq.map (fun project-> DotNetCli.Restore (fun p-> {p with Project=project}))
+    |> Seq.map (DotNet.restore id)
     |> Seq.toArray    
     |> ignore
 )
 
-Target "Build" (fun _ ->
+Target.create "Build" (fun _ ->
     projectFolders
-    |> Seq.map (fun project-> DotNetCli.Build (fun p-> { p with Project=project }))
+    |> Seq.map (DotNet.build id)
     |> Seq.toArray    
     |> ignore        
 )
 
-let release =  ReadFile "RELEASE_NOTES.md" |> ReleaseNotesHelper.parseReleaseNotes
-                
+let release =  File.read "RELEASE_NOTES.md" |> ReleaseNotes.parse
 
-Target "Meta" (fun _ ->
+Target.create "Meta" (fun _ ->
     [ "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">"
       "<PropertyGroup>"
       "<Description>Library for strongly typed websocket use in Fable</Description>"
@@ -62,13 +59,13 @@ Target "Meta" (fun _ ->
       sprintf "<Version>%s</Version>" (string release.SemVer)
       "</PropertyGroup>"
       "</Project>"]
-    |> WriteToFile false "src/Meta.props"    
+    |> File.write false "src/Meta.props"    
 )
 
-Target "Package" (fun _ ->        
-    printfn "%A" currentDirectory
+Target.create "Package" (fun _ ->        
+    printfn "%A" System.Environment.CurrentDirectory
     projectFolders  
-    |> Seq.iter (fun project-> DotNetCli.Pack (fun p-> { p with Project=project; OutputPath = currentDirectory+"/build" }))
+    |> Seq.iter (DotNet.pack (fun p-> { p with OutputPath = Some <| System.Environment.CurrentDirectory + "/build" }))
 )
 
 
@@ -81,4 +78,4 @@ Target "Package" (fun _ ->
     ==> "Package"
 
 // start build
-RunTargetOrDefault "Build"
+Target.runOrDefault "Build"
